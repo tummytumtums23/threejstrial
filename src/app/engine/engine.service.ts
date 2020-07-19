e1f99b5a060ab846f4af0437e7e4fe64bd4f93d4
Aryeman <aryeman@gmail.com>
2020-07-18 18:36:37 -0700

    first commit

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -11,7 +11,7 @@
     private scene: THREE.Scene;
     private light: THREE.AmbientLight;
     private controls : OrbitControls;
-    private cube: THREE.Mesh;
+    private sphere: THREE.Mesh;
     private keylight : THREE.DirectionalLight;
     private filllight : THREE.DirectionalLight;
     private backlight : THREE.DirectionalLight;
@@ -26,10 +26,11 @@
     }
 
     public createScene(canvas: ElementRef<HTMLCanvasElement>): void {
-      console.info("Invoked");
+     
       // The first step is to get the reference of the canvas element from our HTML document
       this.canvas = canvas.nativeElement;
       this.scene = new THREE.Scene();
+      //this.scene.background = new THREE.Color( 0xff0000 );
       this.camera = new THREE.PerspectiveCamera(
         75, window.innerWidth / window.innerHeight, 0.1, 1000
       );
@@ -38,11 +39,20 @@
         alpha: true,    // transparent background
         antialias: true // smooth edges
       });
+      this.renderer.setClearColor( 0xffffff, 0);
       this.renderer.setSize(window.innerWidth, window.innerHeight);
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      
-      this.camera.position.z = 2000  ;
+      const geometry = new THREE.SphereGeometry(1, 32, 32);
+      const material = new THREE.MeshPhongMaterial();
+      this.sphere = new THREE.Mesh( geometry, material );
+      this.camera.position.z = 3;
+      const light = new THREE.DirectionalLight(0xcccccc, 1);
+
+      light.position.set(5, 3, 5);
+       this.scene.add(light);
+     /* var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+      this.scene.add( ambientLight );
+      var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+      this.camera.add(pointLight);
       this.scene.add(this.camera);
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
       this.controls.enableDamping = true;
@@ -55,19 +65,19 @@
       this.filllight.position.set(100, 0, 100);
       this.backlight = new THREE.DirectionalLight(0xffffff, 1.0 );
       this.backlight.position.set(-100, 0, 100).normalize();
+*/
 
-
-      this.scene.add( this.keylight );
+    /*  this.scene.add( this.keylight );
       this.scene.add( this.filllight );
       this.scene.add( this.backlight);
+*/
 
-
-      // soft white light
+      /*// soft white light
       this.light = new THREE.AmbientLight( 0x404040 );
       this.light.position.z = 10;
-      this.scene.add(this.light);
-
-      /*this.cube = new THREE.Mesh( geometry, material );*/
+      this.scene.add(this.light);*/
+    this.animate();
+  /*
       let loader = new GLTFLoader();
     loader.load( 'assets/Earth/Earth.glb', function ( gltf ) {
 
@@ -77,15 +87,18 @@
 
     //console.error( error );
 
-  } );
+  } );*/
      
 
     }
 
-    public animate(): void {
+    public animate = () => {
+      requestAnimationFrame(this.animate);
+       this.renderer.render(this.scene, this.camera);
+    
       // We have to run this outside angular zones,
       // because it could trigger heavy changeDetection cycles.
-      this.ngZone.runOutsideAngular(() => {
+     /* this.ngZone.runOutsideAngular(() => {
         if (document.readyState !== 'loading') {
           this.render();
         } else {
@@ -97,18 +110,19 @@
         window.addEventListener('resize', () => {
           this.resize();
         });
-      });
-    }
+      });*/
 
-   public render(): void {
+    };
+
+
+  /* public render(): void {
       this.frameId = requestAnimationFrame(() => {
         this.render();
       });
      /* Object3d.rotation()
       this.cube.rotation.x += 0.01;
       this.cube.rotation.y += 0.01;*/
-      this.renderer.render(this.scene, this.camera);
-    }
+     
 
     public resize(): void {
       const width = window.innerWidth;
@@ -116,6 +130,7 @@
 
       this.camera.aspect = width / height;
       this.camera.updateProjectionMatrix();
+      
 
       this.renderer.setSize( width, height );
     }
